817fa9a172a1b1e0c65812f1808e5ac0737976b4
milton3128 <mannmilton6@gmail.com>
2024-08-27 18:05:45 -0400

    Completed Assignment

---
+++ Iteration Statement/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace Iteration_Statement;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+        
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i < 0; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+            
+    }
+}*/
